02b7967a8f46c41e58d87c7603ca43d218a74568
Kitty Gordon <kittygordon@Kittys-MacBook-Air.local>
2019-03-19 14:19:17 -0500

    intermediate change

--- assets/javascript/katlogic.js.js
+++ assets/javascript/katlogic.js.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
 
-    function getArtistinfo
-    var = queryURL = "https://itunes.apple.com/search?term=jack+johnson&limit=25";
-         $.get("https://cors-ut-bootcamp.herokuapp.com/https://itunes.apple.com/search?term=artistfirstname+artistlastnme&limit=25", function (res) {
-        console.log(JSON.parse(res));
- //search by artist
-"search?term=firstname+lastname&limit=25"
- //search by album
-"notsurehowtosearchbyalbum"
-        $.get(queryUrl).then(function(response) {
-    // Clear the old info if this is the first page
-    if(page === "0") {
-    // Clear the data
-    concertList = [];
-    
-    
-    
-    });
-         });
+     //search by artist
+            // "search?term=firstname+lastname&limit=25"
+            var artistName = "Britney Spears";
+            $.get("https://cors-ut-bootcamp.herokuapp.com/https://itunes.apple.com/search?term="+artistName+"&limit=25", function (res) {
+               var artistResult = JSON.parse(res).results;
+               console.log(artistResult);
+            })
+                //search by album
+                // "notsurehowtosearchbyalbum"3
+                // $.get(queryURL).then(function (response) {
+                //         // Clear the old info if this is the first page
+                //         if (page === "0") {
+                //             // Clear the data
+                //             concertList = [];
+                        
+                //         }
+
+
+
+
+
+                // });
+});
