f2d465d60e461b14776a91dabd58044702d53dae
Kitty Gordon <kittygordon@Kittys-MacBook-Air.local>
2019-03-19 13:24:13 -0500

    updating

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -80,24 +80,32 @@ $(document).ready(function () {
                 })
             }
 
-            //function getArtistinfo
-            var queryURL = "https://itunes.apple.com/search?term=jack+johnson&limit=25";
-            $.get("https://cors-ut-bootcamp.herokuapp.com/https://itunes.apple.com/search?term=artistfirstname+artistlastnme&limit=25", function (res) {
-                    console.log(JSON.parse(res));
-                    //search by artist
-                    // "search?term=firstname+lastname&limit=25"
-                    //search by album
-                    // "notsurehowtosearchbyalbum"
-                    $.get(queryURL).then(function (response) {
-                            // Clear the old info if this is the first page
-                            if (page === "0") {
-                                // Clear the data
-                                concertList = [];
-                            }
+            //maybe write a function here 
+            var artistName = "Britney Spears";
+            $.get("https://cors-ut-bootcamp.herokuapp.com/https://itunes.apple.com/search?term="+artistName+"&limit=25", function (res) {
+             
+            
+               console.log(JSON.parse(res));
+
+               var artistResult = JSON.parse(res).results;
+               console.log(artistResult);
+                //search by artist
+                // "search?term=firstname+lastname&limit=25"
+                //search by album
+                // "notsurehowtosearchbyalbum"
+                // $.get(queryURL).then(function (response) {
+                //         // Clear the old info if this is the first page
+                //         if (page === "0") {
+                //             // Clear the data
+                //             concertList = [];
+                        
+                //         }
 
 
 
-                    });
+
+
+                // });
             });
 
 
