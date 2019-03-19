a8d55647a3e7420424622c67dee05bc3c34ccb7c
Eric Chao <47439716+echao2012@users.noreply.github.com>
2019-03-19 14:55:39 -0500

    Update logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -80,29 +80,4 @@ $(document).ready(function () {
                 })
             }
 
-<<<<<<< HEAD
-=======
-            //function getArtistinfo
-            var queryURL = "https://itunes.apple.com/search?term=jack+johnson&limit=25";
-            $.get("https://cors-ut-bootcamp.herokuapp.com/https://itunes.apple.com/search?term=artistfirstname+artistlastnme&limit=25", function (res) {
-                    console.log(JSON.parse(res));
-                    //search by artist
-                    // "search?term=firstname+lastname&limit=25"
-                    //search by album
-                    // "notsurehowtosearchbyalbum"
-                    $.get(queryURL).then(function (response) {
-                            // Clear the old info if this is the first page
-                            if (page === "0") {
-                                // Clear the data
-                                concertList = [];
-                            }
-
-
-
-                    });
-            });
-
->>>>>>> origin
-
-
 })
